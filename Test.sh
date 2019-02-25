01476bd1a0b1bdbf46dd8737bdd1524f46caed1b
omi-dev-one <contact.omi@protonmail.com>
2019-02-24 23:39:06 -0600

    First run

--- Test.sh
+++ Test.sh
@@ -3,11 +3,12 @@ set -o pipefail
 
 tshark \
   -r "Directory/Data/Nasdaq/Ise.OrderComboFeed.v1.1/StrategyTradingActionMessage.H.pcap" \
-  -X "lua_script:Nasdaq/Nasdaq.Ise.OrderFeed.Itch.v1.1.Script.Dissector.lua" \
-  -T json > Nasdaq.Ise.OrderFeed.Itch.v1.1.StrategyTradingActionMessage.H.json
+  -X "lua_script:Nasdaq/Nasdaq.Ise.OrderComboFeed.Itch.v1.1.Script.Dissector.lua" \
+  -T json \
+  | jq ".[0]._source.layers.lua" > Nasdaq.Ise.OrderComboFeed.Itch.v1.1.StrategyTradingActionMessage.H.json
 
-cat Nasdaq.Ise.OrderFeed.Itch.v1.1.StrategyTradingActionMessage.H.json
+cat Nasdaq.Ise.OrderComboFeed.Itch.v1.1.StrategyTradingActionMessage.H.json
   
-grep "nasdaq.ise.ordercombofeed.itch.v1.1.strategyid" Nasdaq.Ise.OrderFeed.Itch.v1.1.StrategyTradingActionMessage.H.json 
-grep "nasdaq.ise.ordercombofeed.itch.v1.1.currenttradingstate" Nasdaq.Ise.OrderFeed.Itch.v1.1.StrategyTradingActionMessage.H.json 
+grep "nasdaq.ise.ordercombofeed.itch.v1.1.strategyid" Nasdaq.Ise.OrderComboFeed.Itch.v1.1.StrategyTradingActionMessage.H.json 
+grep "nasdaq.ise.ordercombofeed.itch.v1.1.currenttradingstate" Nasdaq.Ise.OrderComboFeed.Itch.v1.1.StrategyTradingActionMessage.H.json 
   
