881437d89d6e093972dc88cf6c324d91aa000963
omi-dev-one <contact.omi@protonmail.com>
2019-02-24 23:28:26 -0600

    Try 5

--- Test.sh
+++ Test.sh
@@ -5,7 +5,9 @@ tshark \
   -r "Directory/Data/Nasdaq/Ise.OrderComboFeed.v1.1/StrategyTradingActionMessage.H.pcap" \
   -X "lua_script:Nasdaq/Nasdaq.Ise.OrderFeed.Itch.v1.1.Script.Dissector.lua" \
   -T json \
-  | jq ".[0]._source.layers.lua" > Nasdaq.Ise.OrderFeed.Itch.v1.1.StrategyTradingActionMessage.H.json 
+  | jq ".[0]._source.layers.lua" > Nasdaq.Ise.OrderFeed.Itch.v1.1.StrategyTradingActionMessage.H.json
+
+cat Nasdaq.Ise.OrderFeed.Itch.v1.1.StrategyTradingActionMessage.H.json
   
 grep "nasdaq.ise.ordercombofeed.itch.v1.1.strategyid" Nasdaq.Ise.OrderFeed.Itch.v1.1.StrategyTradingActionMessage.H.json 
 grep "nasdaq.ise.ordercombofeed.itch.v1.1.currenttradingstate" Nasdaq.Ise.OrderFeed.Itch.v1.1.StrategyTradingActionMessage.H.json 
