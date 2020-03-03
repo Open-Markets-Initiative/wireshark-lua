502ed7561ae82fb7f1914286ce1f61746c070ce1
omi-dev-one <contact.omi@protonmail.com>
2020-03-02 21:30:42 -0600

    Data repository for tests

--- Test.sh
+++ Test.sh
@@ -2,7 +2,7 @@ set -o errexit
 set -o pipefail
 
 tshark \
-  -r "Directory/Data/Nasdaq/Ise.OrderComboFeed.v1.1/StrategyTradingActionMessage.H.pcap" \
+  -r "Data/Nasdaq/Ise.OrderComboFeed.v1.1/StrategyTradingActionMessage.H.pcap" \
   -X "lua_script:Nasdaq/Nasdaq.Ise.OrderComboFeed.Itch.v1.1.Script.Dissector.lua" \
   -T json \
   | jq '.[0]._source.layers["nasdaq.ise.ordercombofeed.itch.v1.1.lua"]' \
