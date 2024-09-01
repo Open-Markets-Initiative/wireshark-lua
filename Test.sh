d2ddfd98467df4fdc26432c83fc2694d9f84588c
Open-Markets-Initiative <contact.omi@protonmail.com>
2024-09-01 09:56:20 -0500

    Update test file

--- Test.sh
+++ Test.sh
@@ -3,7 +3,7 @@ set -o pipefail
 
 tshark \
   -r "Data/Nasdaq/Ise.OrderComboFeed.v1.1/StrategyTradingActionMessage.H.pcap" \
-  -X "lua_script:Nasdaq/Nasdaq.Ise.OrderComboFeed.Itch.v1.1.Script.Dissector.lua" \
+  -X "lua_script:Nasdaq/Nasdaq_Ise_OrderComboFeed_Itch_v1_1_Dissector.lua" \
   -T json \
   | jq '.[0]._source.layers["nasdaq.ise.ordercombofeed.itch.v1.1.lua"]' \
   > Nasdaq.Ise.OrderComboFeed.Itch.v1.1.StrategyTradingActionMessage.H.json
