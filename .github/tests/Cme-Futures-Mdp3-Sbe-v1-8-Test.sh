3f94f062303661674f3c38837f28c91d2ac6612c
Open-Markets-Initiative <contact@openmarketsinitiative.com>
2026-03-22 17:11:37 -0500

    Update tests

--- .github/tests/Cme-Futures-Mdp3-Sbe-v1-8-Test.sh
+++ .github/tests/Cme-Futures-Mdp3-Sbe-v1-8-Test.sh
@@ -2,15 +2,6 @@ set -o errexit
 set -o pipefail
 
 tshark \
-  -r "omi-data-packets/Cme/Mdp3.Sbe.v1.8/MdIncrementalRefreshSessionStatistics.pcap" \
-  -X "lua_script:Cme/Cme_Futures_Mdp3_Sbe_v1_8_Dissector.lua" \
-  -T json \
-  > Cme.Futures.Mdp3.Sbe.v1.8.MdIncrementalRefreshSessionStatistics.json
-
-grep "cme.futures.mdp3.sbe.v1.8.transacttime" Cme.Futures.Mdp3.Sbe.v1.8.MdIncrementalRefreshSessionStatistics.json
-grep "cme.futures.mdp3.sbe.v1.8.matcheventindicator" Cme.Futures.Mdp3.Sbe.v1.8.MdIncrementalRefreshSessionStatistics.json
-grep "cme.futures.mdp3.sbe.v1.8.padding2" Cme.Futures.Mdp3.Sbe.v1.8.MdIncrementalRefreshSessionStatistics.json
-tshark \
   -r "omi-data-packets/Cme/Mdp3.Sbe.v1.8/MdIncrementalRefreshTradeSummary.pcap" \
   -X "lua_script:Cme/Cme_Futures_Mdp3_Sbe_v1_8_Dissector.lua" \
   -T json \
@@ -19,12 +10,3 @@ tshark \
 grep "cme.futures.mdp3.sbe.v1.8.transacttime" Cme.Futures.Mdp3.Sbe.v1.8.MdIncrementalRefreshTradeSummary.json
 grep "cme.futures.mdp3.sbe.v1.8.matcheventindicator" Cme.Futures.Mdp3.Sbe.v1.8.MdIncrementalRefreshTradeSummary.json
 grep "cme.futures.mdp3.sbe.v1.8.padding2" Cme.Futures.Mdp3.Sbe.v1.8.MdIncrementalRefreshTradeSummary.json
-tshark \
-  -r "omi-data-packets/Cme/Mdp3.Sbe.v1.8/MdIncrementalRefreshVolume.pcap" \
-  -X "lua_script:Cme/Cme_Futures_Mdp3_Sbe_v1_8_Dissector.lua" \
-  -T json \
-  > Cme.Futures.Mdp3.Sbe.v1.8.MdIncrementalRefreshVolume.json
-
-grep "cme.futures.mdp3.sbe.v1.8.transacttime" Cme.Futures.Mdp3.Sbe.v1.8.MdIncrementalRefreshVolume.json
-grep "cme.futures.mdp3.sbe.v1.8.matcheventindicator" Cme.Futures.Mdp3.Sbe.v1.8.MdIncrementalRefreshVolume.json
-grep "cme.futures.mdp3.sbe.v1.8.padding2" Cme.Futures.Mdp3.Sbe.v1.8.MdIncrementalRefreshVolume.json
