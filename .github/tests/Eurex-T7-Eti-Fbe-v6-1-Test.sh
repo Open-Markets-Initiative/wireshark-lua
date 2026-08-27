6e970acd5d468980a171de7e7f25e44d56ab8333
Open-Markets-Initiative <contact@openmarketsinitiative.com>
2026-08-26 23:03:42 -0500

    Regenerate with better decimal prefs

--- .github/tests/Eurex-T7-Eti-Fbe-v6-1-Test.sh
+++ .github/tests/Eurex-T7-Eti-Fbe-v6-1-Test.sh
@@ -5,6 +5,7 @@ chown -R tester:tester .
 
 runuser -u tester -- tshark \
   -r "omi-data-packets/Eurex/T7.Eti.v6.1/Heartbeat.pcap" \
+  --disable-protocol eti \
   -X "lua_script:Eurex/Eurex_T7_Eti_Fbe_v6_1_Dissector.lua" \
   -T json \
   > Eurex.T7.Eti.Fbe.v6.1.Heartbeat.json 2> Eurex.T7.Eti.Fbe.v6.1.Heartbeat.json.stderr \
@@ -14,6 +15,7 @@ grep "eurex.t7.eti.fbe.v6.1.networkmsgid" Eurex.T7.Eti.Fbe.v6.1.Heartbeat.json
 grep "eurex.t7.eti.fbe.v6.1.pad2" Eurex.T7.Eti.Fbe.v6.1.Heartbeat.json
 runuser -u tester -- tshark \
   -r "omi-data-packets/Eurex/T7.Eti.v6.1/RetransmitMeMessageRequest.pcap" \
+  --disable-protocol eti \
   -X "lua_script:Eurex/Eurex_T7_Eti_Fbe_v6_1_Dissector.lua" \
   -T json \
   > Eurex.T7.Eti.Fbe.v6.1.RetransmitMeMessageRequest.json 2> Eurex.T7.Eti.Fbe.v6.1.RetransmitMeMessageRequest.json.stderr \
@@ -29,6 +31,7 @@ grep "eurex.t7.eti.fbe.v6.1.applendmsgid" Eurex.T7.Eti.Fbe.v6.1.RetransmitMeMess
 grep "eurex.t7.eti.fbe.v6.1.pad1" Eurex.T7.Eti.Fbe.v6.1.RetransmitMeMessageRequest.json
 runuser -u tester -- tshark \
   -r "omi-data-packets/Eurex/T7.Eti.v6.1/UserLoginResponse.pcap" \
+  --disable-protocol eti \
   -X "lua_script:Eurex/Eurex_T7_Eti_Fbe_v6_1_Dissector.lua" \
   -T json \
   > Eurex.T7.Eti.Fbe.v6.1.UserLoginResponse.json 2> Eurex.T7.Eti.Fbe.v6.1.UserLoginResponse.json.stderr \
